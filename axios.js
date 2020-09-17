5c8a14b925816e710d4b7fccd4cadb25e7aed596
Maisha Umama <umamamaisha@gmail.com>
2020-09-17 10:39:52 +0600

    sequential axios req

--- axios.js
+++ axios.js
@@ -1,9 +1,24 @@
-axios
-.get('https://swapi.dev/api/planets/')
-.then((res) => {
-    console.log(res.data);
-})
-.catch ((err) => {
-    console.log("IN CATCH CALLBACK!!!");
-    console.log(err);
-});
+const fetchNextPlanet = (url ='https://swapi.dev/api/planets/') => {
+    console.log(url);
+    return axios.get(url);
+};
+const printPlanets =({data}) => {
+    console.log(data);
+    for(let planet of data.results){
+        console.log(planet.name);
+}
+
+return Promise.resolve(data.next);
+
+};
+
+fetchNextPlanet()
+  .then(printPlanets)
+  .then(fetchNextPlanet)
+  .then(printPlanets)
+  .then(fetchNextPlanet)
+  .then(printPlanets)
+ .catch((err) => {
+       console.log("IN CATCH CALLBACK!!!");
+  console.log(err);
+ });
